e5c8928ee7b3fb47b1423f23ce57d77d47a2bc63
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2025-06-03 19:38:57 -0700

    Solve challenge 1

--- src/set_1/challenge_1.rs
+++ src/set_1/challenge_1.rs
@@ -1,3 +1,51 @@
-pub fn greet() -> String {
-    "Hello again, world!".to_string()
+fn char_to_hex(c: &u8) -> Result<u8, String> {
+    match c {
+        b'0'..=b'9' => Ok(c - b'0'),
+        b'a'..=b'f' => Ok(c - b'a' + 10),
+        b'A'..=b'F' => Ok(c - b'A' + 10),
+        _ => Err(format!("Invalid hex char: {}", *c as char))
+    }
+}
+
+fn b64_to_char(v: u8) -> u8 {
+    match v {
+        0..=25 => b'A' + v,
+        26..=51 => b'a' + (v - 26),
+        52..=61 => b'0' + (v - 52),
+        62 => b'+',
+        63 => b'/',
+        _ => b'?'
+    }
+}
+
+fn convert_chunk(chunk: &[u8]) -> [u8; 2] {
+    let left = (chunk[0] << 2) | (chunk[1] >> 2);
+    let right = ((chunk[1] & 0b11) << 4) | chunk[2];
+    [left, right]
+}
+
+pub fn convert(s: String) -> Result<String, String> {
+    use itertools::Itertools;
+
+    let hex_values: Vec<u8> = s.as_bytes()
+        .iter()
+        .map(char_to_hex)
+        .collect::<Result<Vec<u8>, String>>()?;
+
+    let b64_bytes: Vec<u8> = hex_values
+        .into_iter()
+        .chunks(3)
+        .into_iter()
+        .flat_map(|chunk| {
+            let chunk: Vec<u8> = chunk.collect();
+            if chunk.len() != 3 {
+                vec![] 
+            } else {
+                convert_chunk(&chunk).to_vec()
+            }
+        })
+        .map(b64_to_char)
+        .collect();
+
+    Ok(String::from_utf8(b64_bytes).unwrap())
 }
--- tests/test_solutions.rs
+++ tests/test_solutions.rs
@@ -2,5 +2,7 @@ use cryptopals_rs::set_1::challenge_1;
 
 #[test]
 fn test_challenge_1() {
-    assert_eq!(challenge_1::greet(), "Hello again, world!");
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    assert_eq!(challenge_1::convert(input.to_string()).unwrap(), expected_output.to_string());
 }
